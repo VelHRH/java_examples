59787faab6adddf5ca4812301128b1bae862e085
Yurchenko Valentyn <valentyn.iurchenko@nure.ua>
2024-05-26 17:45:40 +0300

    first controller

---
+++ SpringBootMVC/src/main/java/com/example/SpringBootMVC/controller/JobController.java
@@ -0,0 +1,20 @@
+package com.example.SpringBootMVC.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+    
+	@RequestMapping({"/","/home"})
+	public String home() {
+		return "home";
+	}
+}
